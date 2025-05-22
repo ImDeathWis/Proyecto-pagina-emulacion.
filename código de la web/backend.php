d204cf34a509a4fb62ec77e2da661171021eae00
05Luismi <167286990+05Luismi@users.noreply.github.com>
2025-05-22 09:35:33 +0200

    Delete código de la web directory

--- código de la web/backend.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = htmlspecialchars($_POST["nombre"]);
-    $email = htmlspecialchars($_POST["email"]);
-
-    // Aquí puedes conectar a MySQL, guardar datos, o enviar correos
-    echo "¡Hola $nombre! Tu correo ($email) ha sido registrado.";
-}
-?>
